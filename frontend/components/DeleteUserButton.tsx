d4360cffda075fb45a4413e6140868880b2e308d
Yuri Yakovlev <hi@mynameisyuri.com>
2018-09-10 02:12:40 +0200

    Add DeleteUserButton component

---
+++ frontend/components/DeleteUserButton.tsx
@@ -0,0 +1,54 @@
+import { message } from 'antd';
+import gql from 'graphql-tag';
+import remove from 'lodash/remove';
+import * as React from 'react';
+import { Mutation } from 'react-apollo';
+
+import { GET_USER_LIST } from '../pages/dashboard';
+
+interface Props {
+  id: string;
+}
+
+const DELETE_USER = gql`
+  mutation deleteUser($id: ID!) {
+    deleteUser(id: $id) {
+      id
+    }
+  }
+`;
+
+class DeleteUserButton extends React.Component<Props> {
+  public onButtonClick = async deleteUser => {
+    const deleted = await deleteUser({
+      variables: { id: this.props.id },
+    });
+
+    if (deleted) {
+      message.success('User removed');
+    }
+  };
+
+  public render() {
+    return (
+      <Mutation
+        mutation={DELETE_USER}
+        update={(cache, { data: { deleteUser } }) => {
+          const data: any = cache.readQuery({ query: GET_USER_LIST });
+          remove(data.users, { id: deleteUser.id });
+          cache.writeQuery({ query: GET_USER_LIST, data });
+        }}
+      >
+        {(deleteUser, { error }) => {
+          if (error) {
+            return <p>Error :(</p>;
+          }
+
+          return <a onClick={() => this.onButtonClick(deleteUser)}>delete</a>;
+        }}
+      </Mutation>
+    );
+  }
+}
+
+export default DeleteUserButton;
